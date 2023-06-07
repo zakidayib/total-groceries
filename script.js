56e284b205b479c815c9e28b4a248f49f2819a32
zaki tayyip <zakidayib1997@gmail.com>
2023-06-07 14:41:07 +0300

    totalprice-solved

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 // Good Luck 💪🏾
+
+/*
+    ## Total price of groceries 
+
+##### Your Task 📚:
+
+Create a function that takes an array of objects (groceries) which calculates the total price and returns it as a number. A grocery object has a product, a quantity and a price, for example:
+
+```
+{
+  "product": "Milk",
+  "quantity": 1,
+  "price": 1.50
+}
+```
+
+##### Examples 😃:
+
+```
+// 1 bottle of milk:
+getTotalPrice([
+  { product: "Milk", quantity: 1, price: 1.50 }
+]) ➞ 1.5
+
+// 1 bottle of milk & 1 box of cereals:
+getTotalPrice([
+  { product: "Milk", quantity: 1, price: 1.50 },
+  { product: "Cereals", quantity: 1, price: 2.50 }
+]) ➞ 4
+
+// 3 bottles of milk:
+getTotalPrice([
+  { product: "Milk", quantity: 3, price: 1.50 }
+]) ➞ 4.5
+```
+
+Good Luck 😀
+*/
+
+function getTotalPrice(grocery){
+    let total = 0;
+
+    for(let i=0; i<grocery.length; i++){
+        total = total + grocery[i].quantity*grocery[i].price;
+    }
+
+    return total;
+}
+
+
+
+  console.log(getTotalPrice([
+    { product: "Milk", quantity: 3, price: 1.50 }
+  ]))
